e1234c0b9d42f5f317a17eb85f458f8b2c4aa74f
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-11-09 23:51:13 -0300

    Se agrega lista con opcion para agregar y borrar elementos

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [accountValue, setAccountValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [accountList, setAccountList] = useState([]);
+
+  const handleChangeTextAccount = (value) => {
+    setAccountValue(value); 
+  }
+
+  const handleChangeTextPassword = (value) => {
+   setPasswordValue(value); 
+  }
+
+  const handleAddAccount = () => {
+    const newAccount = {
+      account: accountValue,
+      password:passwordValue,
+      id: Math.random().toString(),
+    };
+    setAccountList([
+      ...accountList, newAccount,
+    ]);
+    setAccountValue('');
+    setPasswordValue('')
+  }
+
+  const handleRemoveAccount = (id) => {
+   const newList = accountList.filter(item => item.id !== id);
+   setAccountList(newList)
+  }
+  
   return (
+    
     <View style={styles.container}>
-      <Text>Hola, Coder! Soy Sebastian</Text>
+      <Text>AGREGA TU CUENTA</Text>
       <StatusBar style="auto" />
+      <View style={styles}>
+        <TextInput
+          placeholder="Cuenta"
+          style={styles.inputContainer}
+          onChangeText={handleChangeTextAccount}
+          value={accountValue}
+        /><TextInput
+        placeholder="Password"
+        style={styles.inputContainer}
+        onChangeText={handleChangeTextPassword}
+        value={passwordValue}
+      />
+        <Button
+          title="Agregar"onPress={handleAddAccount}
+        />
+      </View>
+      <View>
+        <FlatList
+          data={accountList}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <View style={styles} key={item.id}>
+              <Text> Cuenta: {item.account} </Text>
+              <Text> Password: {item.password}</Text> 
+              <Button title="X" onPress={() => handleRemoveAccount(item.id)} />
+            </View>
+          )}
+        />
+      </View>
     </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+   
+    backgroundColor: 'rgb(212, 214, 185)',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    
   },
 });
